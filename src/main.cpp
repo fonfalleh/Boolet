c22b4c3fa28a82ca6c889834c31787fc3feff3b7
Jacob <jacob.rippe@gmail.com>
2015-02-12 17:49:39 +0100

    Restarting this project. Kinda.

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include "drawhandler.h"
+#include "utils.h"
+#include "scene.h"
+
+int main()
+{
+    // Init
+    const int height = 315, width = 420;
+    sf::RenderWindow window(sf::VideoMode(width, height), "BOOLET");
+    window.setFramerateLimit(200); //TODO Idunnolols
+    DrawHandler drawer;
+    Scene scene(width, height, &drawer, &window);
+
+    //Timekeeping
+    sf::Clock* timer = new sf::Clock();
+    sf::Time step = sf::milliseconds(1000) / 60.0f; //60 fps //Can't quite go below 60? Is drawing capped by hardware?
+    sf::Time elapsed;
+    while (window.isOpen())
+    {
+        elapsed += timer->restart();
+        if(elapsed < step)
+            continue;
+        elapsed -= step;
+
+        // Magic stuff. Doesn't work wihout it.
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        scene.update();
+    }
+    return 0;
+}
